abbb6853feb725c470a586e992f4633cdcb5e7f5
Joeylene Rivera <joeylenerivera@gmail.com>
2021-04-02 01:06:11 +0800

    feat: add new game button

--- components/Home/HomePlay/index.tsx
+++ components/Home/HomePlay/index.tsx
@@ -1,43 +1,57 @@
 import React from 'react';
-import { Box, Heading } from 'minorui';
+import { Box, Button, Heading } from 'minorui';
 import { usePlayerData } from '@/models/PlayerData';
 import { Counter } from '@/components/Counter';
 
 export function HomePlay() {
-  const [players, setPlayers] = usePlayerData((state) => [
+  const [players, setPlayers, reset] = usePlayerData((state) => [
     state.players,
     state.setPlayers,
+    state.reset,
   ]);
 
   return (
-    <Box
-      css={{
-        m: '$100',
-        display: 'grid',
-        gridTemplateColumns: '1fr 1fr',
-        gap: '$100',
-
-        '@downSm': {
-          gridTemplateColumns: '1fr',
-        },
-      }}
-    >
-      {players.map((player) => (
-        <Box
-          key={player.id}
-          css={{
-            textAlign: 'center',
-          }}
+    <Box css={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+      <Box css={{ mb: '$200' }}>
+        <Button
+          type="button"
+          size="md"
+          kind="tertiary"
+          onClick={reset}
+          css={{ px: '$200', py: '$50' }}
         >
-          <Heading as="h2" size="2xl">
-            {player.name}
-          </Heading>
-          <Counter
-            player={player}
-            setPlayers={setPlayers}
-          />
-        </Box>
-      ))}
+          New Game
+        </Button>
+      </Box>
+      <Box
+        css={{
+          m: '$100',
+          display: 'grid',
+          gridTemplateColumns: '1fr 1fr',
+          gap: '$100',
+
+          '@downSm': {
+            gridTemplateColumns: '1fr',
+          },
+        }}
+      >
+        {players.map((player) => (
+          <Box
+            key={player.id}
+            css={{
+              textAlign: 'center',
+            }}
+          >
+            <Heading as="h2" size="2xl">
+              {player.name}
+            </Heading>
+            <Counter
+              player={player}
+              setPlayers={setPlayers}
+            />
+          </Box>
+        ))}
+      </Box>
     </Box>
   );
 }
